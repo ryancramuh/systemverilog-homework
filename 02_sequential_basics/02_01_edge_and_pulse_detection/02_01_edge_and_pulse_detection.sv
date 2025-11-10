752012c193674716163e9cdf6394806308e3685a
Ryan Cramer <ryancramer22@gmail.com>
2025-11-09 23:35:36 -0800

    solved 2.1 and 2.2

--- 02_sequential_basics/02_01_edge_and_pulse_detection/02_01_edge_and_pulse_detection.sv
+++ 02_sequential_basics/02_01_edge_and_pulse_detection/02_01_edge_and_pulse_detection.sv
@@ -31,6 +31,27 @@ module one_cycle_pulse_detector (input clk, rst, a, output detected);
   //
   // Note:
   // See the testbench for the output format ($display task).
+  logic a_r;
+  logic a_f;
 
-
+  always_ff@(posedge clk)
+    if(rst)
+      a_r <= '0;
+    else
+      a_r <= a;
+  
+  always_ff@(posedge clk)
+    if(rst)
+      a_f <= '0;
+    else if(a_r)
+      a_f <= '0;
+    else
+      a_f <= a;
+    
+  assign detected = a_f & ~a;
+  
+  
 endmodule
+/*seq_a:
+1000_1110_0100_0100
+0000_0000_1000_1000*/
--- 02_sequential_basics/02_02_single_and_double_rate_fibonacci/02_02_single_and_double_rate_fibonacci.sv
+++ 02_sequential_basics/02_02_single_and_double_rate_fibonacci/02_02_single_and_double_rate_fibonacci.sv
@@ -34,5 +34,18 @@ module fibonacci_2
   // Task:
   // Implement a module that generates two fibonacci numbers per cycle
 
+logic [15:0] temp1, temp2;
+
+    always_ff @ (posedge clk)
+        if (rst)
+          { num, temp1 } <= { 16'd1, 16'd1 };
+        else
+          { num, temp1 } <= { num + temp1, num + (num2 + temp1) };
+    
+    always_ff @(posedge clk)
+        if(rst)
+          { num2, temp2 } <= { 16'd1, 16'd2};
+        else
+          { num2, temp2 } <= { temp1 + temp2, temp2 + (num2 + temp2)};
 
 endmodule
